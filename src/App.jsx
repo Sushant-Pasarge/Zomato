8e8d62f2163af42379cddfc3d89db7173509de64
Sushant-Pasarge <pasargesushant@gmail.com>
2025-03-28 12:41:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Search from './components/Search';
+
+
+function App() {
+
+  return (
+    <>
+      <Search />
+    </>
+  );
+}
+
+export default App;
